f63abac32249f54ae831b2a6401596292de04da7
vladislav1133 <vladislv1133light@ukr.net>
2017-07-04 17:20:09 +0300

    add migrations

---
+++ database/migrations/2017_07_04_094513_create_filters_talbe.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFiltersTalbe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('filters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('alias',150)->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('filters');
+    }
+}
